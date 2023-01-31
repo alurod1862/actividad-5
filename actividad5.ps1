d012ba451205e77e77739383fe08f2ce95305593
alurod1862 <91328330+alurod1862@users.noreply.github.com>
2023-01-31 19:57:14 +0100

    Update actividad5.ps1

--- actividad5.ps1
+++ actividad5.ps1
@@ -2,14 +2,17 @@ New-LocalGroup -Name grupo1 -Description "grupo1"
 New-LocalGroup -Name grupo2 -Description "grupo2"
 New-LocalGroup -Name grupo3 -Description "grupo3"
 
+$user1 = "user01"
 $clave=ConvertTo-SecureString "passuser01" -AsPlainText -Force
-New-LocalUser "J.Cuantico" -Password $clave -FullName "J.Cuantico" -Description "Usuario1"
+New-LocalUser $user1 -Password $clave -FullName "J.Cuantico" -Description "Usuario1"
+$user2 = "user02"
 $clave=ConvertTo-SecureString "passuser02" -AsPlainText -Force
-New-LocalUser "R.Nvidia" -Password $clave -FullName "R.Nvidia" -Description "Usuario2"
+New-LocalUser $user2 -Password $clave -FullName "R.Nvidia" -Description "Usuario2"
+$user3 = "user03"
 $clave=ConvertTo-SecureString "passuser03" -AsPlainText -Force
-New-LocalUser "L.PuertaLogica" -Password $clave -FullName "L.PuertaLogica" -Description "Usuario3"
+New-LocalUser $user3 -Password $clave -FullName "L.PuertaLogica" -Description "Usuario3"
 
 
-Add-LocalGroupMem -Group "grupo1" -Member user01
-Add-LocalGroupMem -Group "grupo2" -Member user02
-Add-LocalGroupMem -Group "grupo3" -Member user03
+Add-LocalGroupMember -Group "grupo1" -Member user01
+Add-LocalGroupMember -Group "grupo2" -Member user02
+Add-LocalGroupMember -Group "grupo3" -Member user03
